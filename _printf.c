e3f716ea4dae33651b959be74cde23d94275a5e3
Evans262 <65713932+Evans262@users.noreply.github.com>
2021-10-05 14:26:04 -1100

    I'm not going anywhere

--- _printf.c
+++ _printf.c
@@ -12,28 +12,28 @@
 
 int printIdentifiers(char next, va_list arg)
 {
-	int functsIndex;
+int functsIndex;
 
-	identifierStruct functs[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{"u", print_unsigned},
-		{"b", print_unsignedToBinary},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_STR},
-		{NULL, NULL}
-	};
+identifierStruct functs[] = {
+{"c", print_char},
+{"s", print_str},
+{"d", print_int},
+{"i", print_int},
+{"u", print_unsigned},
+{"b", print_unsignedToBinary},
+{"o", print_oct},
+{"x", print_hex},
+{"X", print_HEX},
+{"S", print_STR},
+{NULL, NULL}
+};
 
-	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
-	{
-		if (functs[functsIndex].indentifier[0] == next)
-			return (functs[functsIndex].printer(arg));
-	}
-	return (0);
+for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
+{
+if (functs[functsIndex].indentifier[0] == next)
+return (functs[functsIndex].printer(arg));
+}
+return (0);
 }
 
 /**
@@ -49,44 +49,44 @@ int printIdentifiers(char next, va_list arg)
 
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	int identifierPrinted = 0, charPrinted = 0;
-	va_list arg;
+unsigned int i;
+int identifierPrinted = 0, charPrinted = 0;
+va_list arg;
 
-	va_start(arg, format);
-	if (format == NULL)
-		return (-1);
+va_start(arg, format);
+if (format == NULL)
+return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			charPrinted++;
-			continue;
-		}
-		if (format[i + 1] == '%')
-		{
-			_putchar('%');
-			charPrinted++;
-			i++;
-			continue;
-		}
-		if (format[i + 1] == '\0')
-			return (-1);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+charPrinted++;
+continue;
+}
+if (format[i + 1] == '%')
+{
+_putchar('%');
+charPrinted++;
+i++;
+continue;
+}
+if (format[i + 1] == '\0')
+return (-1);
 
-		identifierPrinted = printIdentifiers(format[i + 1], arg);
-		if (identifierPrinted == -1 || identifierPrinted != 0)
-			i++;
-		if (identifierPrinted > 0)
-			charPrinted += identifierPrinted;
+identifierPrinted = printIdentifiers(format[i + 1], arg);
+if (identifierPrinted == -1 || identifierPrinted != 0)
+i++;
+if (identifierPrinted > 0)
+charPrinted += identifierPrinted;
 
-		if (identifierPrinted == 0)
-		{
-			_putchar('%');
-			charPrinted++;
-		}
-	}
-	va_end(arg);
-	return (charPrinted);
+if (identifierPrinted == 0)
+{
+_putchar('%');
+charPrinted++;
+}
+}
+va_end(arg);
+return (charPrinted);
 }
